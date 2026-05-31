f2e650f1f74872254da72cd327a61e08549b547a
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-31 01:49:19 -0400

    fix: apply solution for issue #508

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,15 +1,3 @@
-"""Memanto integrations for developer tools and workflows."""
-
-from memanto.integrations.mattpocock_skills import (
-    MattpocockSkillsIntegration,
-    SkillContext,
-    SkillExecution,
-    enable_memanto_skills,
-)
-
-__all__ = [
-    "MattpocockSkillsIntegration",
-    "SkillContext",
-    "SkillExecution",
-    "enable_memanto_skills",
-]
+"""
+Integration modules for Memanto.
+"""
